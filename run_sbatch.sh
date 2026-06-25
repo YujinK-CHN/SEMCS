87b4a8d96f202d0182ffad7017bb21b8db984320
Korei <126559264+YujinK-CHN@users.noreply.github.com>
2026-06-25 17:19:36 -0400

    1

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -74,15 +74,15 @@ else
 fi
 
 SCRIPTS=(
-  s_w_comm_w_pred.sh    # MCS
+  #s_w_comm_w_pred.sh    # MCS
   #s_dt2gs.sh            # DT2GS
-  #s_sesil.sh            # SESiL
+  s_sesil.sh            # SESiL
 )
 
 METHODS=(
-  mcs_skill_GRU_Pre_Merge_CommMask    # MCS
+  #mcs_skill_GRU_Pre_Merge_CommMask    # MCS
   #dt2gs_subtask_VAE_Merge             # DT2GS
-  #sesil_enc_VAE_Merge                 # SESiL
+  sesil_enc_VAE_Merge                 # SESiL
 )
 
 # ── Run multi‐task scripts ───────────────────────────────
