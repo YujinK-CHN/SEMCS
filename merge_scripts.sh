81946a090d6c2341af74ad84ad003b92b029cad1
Korei <126559264+YujinK-CHN@users.noreply.github.com>
2026-06-27 11:06:17 -0400

    1

--- merge_scripts.sh
+++ merge_scripts.sh
@@ -146,7 +146,7 @@ elif [ "$algorithm_name" == "dt2gs" ]; then
 elif [ "$algorithm_name" == "sesil" ]; then
     job_id="tE"
     use_recurrent_policy=0
-    experiment_name="${settings}_enc{${num_skills}}_evo{25}"
+    experiment_name="${settings}_enc{${num_skills}}"
     echo "env is ${env_name}, train tasks are ${train_tasks}, eval tasks are ${eval_tasks}, algorithm is ${algorithm_name}, exp key is ${experiment_name}"
 fi
 
