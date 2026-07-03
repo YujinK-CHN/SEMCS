592b627e116617241b5326003e7dde7cbecf5783
Korei <126559264+YujinK-CHN@users.noreply.github.com>
2026-07-03 00:45:34 -0400

    1

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -78,9 +78,9 @@ else
 fi
 
 SCRIPTS=(
-  s_w_comm_w_pred.sh    # MCS
+  #s_w_comm_w_pred.sh    # MCS
   #s_dt2gs.sh            # DT2GS
-  #s_sesil.sh            # SESiL
+  s_sesil.sh            # SESiL
   #s_sesil.sh            # SESiL (no evo, ablation)
   #s_sesil.sh            # SESiL (transformer policy head, ablation)
   #s_sft.sh              # SFT (sequential fine-tuning baseline)
@@ -88,9 +88,9 @@ SCRIPTS=(
 )
 
 METHODS=(
-  mcs_skill_GRU_Pre_Merge_CommMask    # MCS
+  #mcs_skill_GRU_Pre_Merge_CommMask    # MCS
   #dt2gs_subtask_VAE_Merge             # DT2GS
-  #sesil_enc_VAE_Merge                 # SESiL
+  sesil_enc_VAE_Merge                 # SESiL
   #sesil_enc_VAE_Merge_noevo           # SESiL (no evo)
   #sesil_enc_VAE_Merge_trans           # SESiL (transformer)
   #sft_sequential                      # SFT
