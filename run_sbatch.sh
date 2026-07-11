c2644efafa62c05dc3061cb308ef9175d92f68f5
Korei <126559264+YujinK-CHN@users.noreply.github.com>
2026-07-11 16:07:13 -0400

    1

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -86,8 +86,8 @@ SCRIPTS=(
   #s_w_comm_w_pred.sh    # MCS
   #s_dt2gs.sh            # DT2GS
   #s_sft.sh              # SFT (sequential fine-tuning baseline)
-  #s_sft.sh              # Joint MAPPO (multi-task without skills)
-  s_sesil.sh             # SESiL (MAPPO solvers)
+  s_sft.sh              # Joint MAPPO (multi-task without skills)
+  #s_sesil.sh            # SESiL (MAPPO solvers)
   #s_sesil.sh            # SESiL (MCS solvers)
 )
 
@@ -95,8 +95,8 @@ METHODS=(
   #mcs_skill_GRU_Pre_Merge_CommMask    # MCS
   #dt2gs_subtask_VAE_Merge             # DT2GS
   #sft_sequential                      # SFT
-  #joint_mappo                         # Joint MAPPO
-  sesil_mappo                          # SESiL (MAPPO solvers)
+  joint_mappo                         # Joint MAPPO
+  #sesil_mappo                         # SESiL (MAPPO solvers)
   #sesil_mcs                           # SESiL (MCS solvers)
 )
 
