369ab48c8961e0d6b846d3192dc26772b639a481
Korei <126559264+YujinK-CHN@users.noreply.github.com>
2026-09-01 12:57:02 -0400

    1

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -88,11 +88,11 @@ SCRIPTS=(
   #s_dt2gs.sh            # DT2GS
   #s_sft.sh              # SFT (sequential fine-tuning baseline)
   #s_sft.sh              # MAPPO (multi-task without skills)
-  #s_sesil.sh            # SESiL (MAPPO solvers)
+  s_sesil.sh            # SESiL (MAPPO solvers)
   #s_sesil.sh            # SESiL (MCS solvers)
   #s_sesil.sh            # SESiL (DT2GS solvers)
   #s_newskill.sh         # Newskill (MAPPO baseline)
-  s_newskill.sh         # Newskill (SESiL + outlander)
+  #s_newskill.sh         # Newskill (SESiL + outlander)
   #s_sebal.sh            # SEBAL (fixed base)
   #s_sebal.sh            # SEBAL (rolling base)
 )
@@ -102,11 +102,11 @@ METHODS=(
   #dt2gs_subtask_VAE_Merge             # DT2GS
   #sft_sequential                      # SFT
   #mappo                               # MAPPO
-  #sesil_mappo                         # SESiL (MAPPO solvers)
+  sesil_mappo                         # SESiL (MAPPO solvers)
   #sesil_mcs                           # SESiL (MCS solvers)
   #sesil_dt2gs                         # SESiL (DT2GS solvers)
   #newskill_mappo                      # Newskill (MAPPO baseline)
-  newskill_sesil_mappo                # Newskill (SESiL + outlander)
+  #newskill_sesil_mappo                # Newskill (SESiL + outlander)
   #sebal_fixed                         # SEBAL (fixed base)
   #sebal_rolling                       # SEBAL (rolling base)
 )
